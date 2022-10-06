8656c448add532d706f6acb305c28aa2123ed623
Olga <osa_007@mail.ru>
2022-10-06 19:50:07 +0300

    added task4 for HW3

--- Task04.py
+++ Task04.py
@@ -2,3 +2,15 @@
 
 num = int(input('Введите число для преобразования: '))
 print(bin(num).replace("0b",""))
+
+# вариант 2
+
+def transform (n):
+    a = ''
+    while n > 0:
+        a += str(n % 2)  
+        n = n // 2
+    return a
+
+num = int(input('Введите число: '))
+print (transform(num))
