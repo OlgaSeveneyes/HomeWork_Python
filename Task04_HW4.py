b7494543f2f98f9f94d0bd8f1ae05855a63aa694
Olga <osa_007@mail.ru>
2022-10-10 18:48:17 +0300

    added task04 for HW4

--- Task04_HW4.py
+++ Task04_HW4.py
@@ -2,31 +2,31 @@
 # список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k
 
 from random import randint
-import itertools as it
 
+k = int(input("Введите степень k: "))
 
-def ratios_list(k):
-    ratios = [randint(0, 10) for i in range(k + 1)]
-    if ratios[0] == 0:
-        ratios[0] = randint(1, 10)
-    return ratios
+factor = []
+for i in range(1, k +2):
+    factor.append(randint(1, 101))
 
+result = []
+for i in range(len(factor)):
+    if k == 1:
+        result.append(f'{factor[i]}*x')
+    elif k == 0:
+        result.append(f'{factor[i]}')
+    else:
+        result.append(f'{factor[i]}*x^{k}')
+    signs = randint(0, 1)
+    if signs == 1:
+        result.append('+')
+    elif signs == 0:
+        result.append('-')
+    k -= 1
 
-def polynom_list(cof, ratios):
-    op = ['*x^']*(cof-1) + ['*x']
-    polynom = [[a, b, c] for a, b, c in it.zip_longest(
-        ratios, op, range(cof, 1, -1), fillvalue='') if a != 0]
-    for x in polynom:
-        x.append(' + ')
-    polynom = list(it.chain(*polynom))
-    polynom[-1] = ' = 0'
-    return "".join(map(str, polynom)).replace(' 1*x', ' x')
+result.pop(-1)
+result.append('=0')
 
-
-cof = randint(2, 5)
-ratios = ratios_list(cof)
-polynom = polynom_list(cof, ratios)
-print(polynom)
-
-with open('File.txt', 'w') as data:
-    data.write(polynom)
+record = open('data.txt', 'w')
+record.write(''.join(result))
+record.close()
