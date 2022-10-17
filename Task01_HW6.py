676f83941dc1978ae933f9e28e2d7dbdecc3d72f
Olga <osa_007@mail.ru>
2022-10-17 17:24:45 +0300

    added task01 for HW6

---
+++ Task01_HW6.py
@@ -0,0 +1,18 @@
+# Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
+
+import random
+
+num = int(input('Введите число: '))
+list1 = []
+for i in range(1, num+1):
+    list1.append(random.randint(-num, num+1))
+print(list1)
+
+list_new = [i for i in list1 if list1.count(i) == 1]
+print(list_new)
+
+# вариант 2
+
+list_new2 = list(filter(lambda i: list1.count(i) == 1, list1))
+print(list_new2)
